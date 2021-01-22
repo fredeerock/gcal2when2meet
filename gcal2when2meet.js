128d84a7a71aa0102cd98e175fa2a30b612c83ae
Derick Ostrenko <dostrenko@gmail.com>
2021-01-22 09:39:03 -0600

    Update gcal2when2meet.js

--- gcal2when2meet.js
+++ gcal2when2meet.js
@@ -4,8 +4,8 @@ document.body.appendChild(document.createElement('script')).src =
   "https://apis.google.com/js/client.js?onload=GCAL";
 
 // when2meet
-var CLIENT_ID = "380166371492-pqrv7v58aac56h854qujmdvsv2b14455.apps.googleusercontent.com";
-var API_KEY = "AIzaSyBBtM5IlDEK5TaY9G0ypRok8PO9kpNFrCM";
+var CLIENT_ID = "1054975614201-suhimk4get59t2e0g1nifbf7c5rr4c1a.apps.googleusercontent.com";
+var API_KEY = "xLtk_1FV8N1GUGv977e5zony";
 var SCOPES = "https://www.googleapis.com/auth/calendar.readonly";
 var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"];
 var calendars = [];
